3dcde7877936fdd0767688d778e39ee230e84ec7
Aitor <dam19.2021@gmail.com>
2021-02-24 15:31:22 +0100

    implementacion test <testNumeroAlumnos>

--- src/ed/examen/modelo/test/CursoTest.java
+++ src/ed/examen/modelo/test/CursoTest.java
@@ -8,31 +8,31 @@ import ed.examen.modelo.Curso;
 import ed.examen.modelo.Persona;
 
 class CursoTest {
-	
+
 	Curso c = new Curso();
 	Persona p = new Persona("123456789Z", "Aitor", "Careaga");
 	Persona p2 = new Persona("123456789A", "Aito", "Careag");
 
 	@Test
 	void testEliminarAlumno() {
-		//clase valida
-				boolean lanzaException=false;
-				try {
-					c.eliminarAlumno("123456789");
-				} catch (Exception e) {
-					lanzaException =true;
-				}
-				assertFalse(lanzaException);
-				
-				//clase invalida
-				lanzaException=false;
-				try {
-					c.eliminarAlumno("1234567");
-				} catch (Exception e) {
-					lanzaException =true;
-				}
-				assertTrue(lanzaException);
-			}
+		// clase valida
+		boolean lanzaException = false;
+		try {
+			c.eliminarAlumno("123456789");
+		} catch (Exception e) {
+			lanzaException = true;
+		}
+		assertFalse(lanzaException);
+
+		// clase invalida
+		lanzaException = false;
+		try {
+			c.eliminarAlumno("1234567");
+		} catch (Exception e) {
+			lanzaException = true;
+		}
+		assertTrue(lanzaException);
+	}
 
 	@Test
 	void testAniadirAlumno() {
@@ -40,16 +40,16 @@ class CursoTest {
 		int sizeBefore = c.numeroAlumnos();
 		c.aniadirAlumno(p);
 		int sizeAfter = c.numeroAlumnos();
-		
-		assertEquals(sizeBefore, sizeAfter-1);
+
+		assertEquals(sizeBefore, sizeAfter - 1);
 	}
 
 	@Test
 	void testEstaRegistrado() {
-		c.aniadirAlumno(p); 
-		
-		boolean expected=true;
-		
+		c.aniadirAlumno(p);
+
+		boolean expected = true;
+
 		assertEquals(expected, c.estaRegistrado("123456789Z"));
 	}
 
@@ -57,13 +57,16 @@ class CursoTest {
 	void testCurso() {
 		c.aniadirAlumno(p);
 		c.aniadirAlumno(p2);
-		Integer dos=2; 
-		assertEquals(dos,c.numeroAlumnos());
+		Integer dos = 2;
+		assertEquals(dos, c.numeroAlumnos());
 	}
 
 	@Test
 	void testNumeroAlumnos() {
-		fail("Not yet implemented");
+		int lista = c.numeroAlumnos();
+		c.aniadirAlumno(p);
+		int listaNueva = c.numeroAlumnos();
+		assertEquals(lista, listaNueva - 1);
 	}
 
 }
